e4de1c1294cda6dba553f4d09ed04a3add7dff64
ashn <me@ashn.dev>
2021-10-26 21:57:59 -0400

    Cleanup demo program

--- main.py
+++ main.py
@@ -11,14 +11,17 @@ import bpgfx
 class MyAnimatedDrawable:
     SIDE_LENGTH = 5
 
-    def __init__(self, position: bpgfx.Point) -> None:
+    def __init__(self, position: bpgfx.Point, dx=+1, dy=+1) -> None:
         self.position = position
+        self.dx = dx
+        self.dy = dy
         self.textures = [
             bpgfx.Texture(self.SIDE_LENGTH, self.SIDE_LENGTH),
             bpgfx.Texture(self.SIDE_LENGTH, self.SIDE_LENGTH),
             bpgfx.Texture(self.SIDE_LENGTH, self.SIDE_LENGTH),
             bpgfx.Texture(self.SIDE_LENGTH, self.SIDE_LENGTH),
         ]
+        self.counter = 0
         self.index = 0
 
         def edge(n) -> bool:
@@ -31,8 +34,22 @@ class MyAnimatedDrawable:
                 self.textures[2].set_dot(x, y, abs(x - y) > 1 or None)
                 self.textures[3].set_dot(x, y, edge(x) or edge(y) or None)
 
-    def advance(self):
-        self.index = (self.index + 1) % len(self.textures)
+    def update(self, canvas: bpgfx.Canvas, frame: int):
+        self.position.x += self.dx
+        self.position.y += self.dy
+
+        if self.position.x <= 1:
+            self.dx = +1
+        elif self.position.x + self.SIDE_LENGTH >= canvas.width - 1:
+            self.dx = -1
+
+        if self.position.y <= 1:
+            self.dy = +1
+        elif self.position.y + self.SIDE_LENGTH >= canvas.height - 1:
+            self.dy = -1
+
+        if frame % 15 == 0:
+            self.index = (self.index + 1) % len(self.textures)
 
     def draw(self, canvas: bpgfx.Canvas):
         sprite = bpgfx.Sprite(self.position, self.textures[self.index])
@@ -42,31 +59,39 @@ class MyAnimatedDrawable:
 def main() -> None:
     canvas = bpgfx.Canvas(160, 120)
 
-    def wave(t: int) -> int:
-        radians = math.radians(t)
-        scale = (canvas.height * 0.75) / 2
-        return int(canvas.height / 2 - math.sin(radians) * scale)
-
     animated = MyAnimatedDrawable(bpgfx.Point(3, 3))
     boarder = bpgfx.Rectangle(bpgfx.Point(0, 0), canvas.width, canvas.height)
-
-    FPS = 30
-    for i in itertools.count(start=1):
+    for t in itertools.count(start=1):
         canvas.clear()
 
+        # Draw the rectangular boarder around the canvas.
         canvas.draw(boarder)
 
-        for x in range(canvas.width):
-            y = wave((i + x) * 2)
-            canvas.draw(bpgfx.Point(x, canvas.height // 2))
-            canvas.draw(bpgfx.Point(x, y))
+        # Draw the points of the sine wave for all on-canvas (x, y) pairs.
+        def wave(k: int) -> int:
+            radians = math.radians(k * 2)
+            scale = (canvas.height * 0.75) / 2
+            return int(canvas.height / 2 - math.sin(radians) * scale)
 
+        for x in range(canvas.width):
+            canvas.draw(bpgfx.Point(x, canvas.height // 2))  # x-axis
+            canvas.draw(bpgfx.Point(x, wave(t + x)))
+
+        # Draw the animated square as well as the line that follows the
+        # animated square around the canvas.
+        follower = bpgfx.Line(
+            bpgfx.Point(canvas.width // 2, canvas.height // 2),
+            bpgfx.Point(
+                animated.position.x + animated.SIDE_LENGTH // 2,
+                animated.position.y + animated.SIDE_LENGTH // 2,
+            ),
+        )
+        canvas.draw(follower)
         canvas.draw(animated)
-        if i % 15 == 0:
-            animated.advance()
+        animated.update(canvas, t)
 
         print(f"{bpgfx.CLEAR}{canvas}", end="")
-        time.sleep(1 / FPS)
+        time.sleep(1 / 30)
 
 
 if __name__ == "__main__":
