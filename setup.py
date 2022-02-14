69ebe39308b893844e1c91500a6c15023e749ac7
ashn <me@ashn.dev>
2022-02-13 22:07:12 -0500

    Release v1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="braille-pattern-graphics",
-    version="0.1.0",
+    version="1.0.0",
     description="2D graphics using braille patterns",
     author="ashn",
     author_email="<me@ashn.dev>",
